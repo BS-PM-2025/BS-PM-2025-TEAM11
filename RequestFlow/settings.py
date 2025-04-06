2e29101ddaf9ee03ef8a90e791ed6c34546360ec
lena abuabid <lenaab1@ac.sce.ac.il>
2025-04-06 13:41:33 +0300

    fff

--- RequestFlow/settings.py
+++ RequestFlow/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'RequestFlow.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -114,8 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
+STATIC_URL = '/static/'  # This is the URL where the static files will be served from.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where collected static files will be stored.
 
-STATIC_URL = 'static/'
+# If you have custom static files directories:
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Add any additional directories containing static files.
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
